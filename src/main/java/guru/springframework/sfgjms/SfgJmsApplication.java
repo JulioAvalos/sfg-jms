f2bb2f1d01ef22616edfdbfceda08f4603b6a89a
JulioAvalos <julioavalos2@msn.com>
2020-05-13 11:57:12 -0600

    first commit using jms

---
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgjms;
+
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJmsApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        //using the recommended setup for embeeded servers from ActiveMQ Documentation
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+            .setPersistenceEnabled(false)
+            .setJournalDirectory("target/data/journal")
+            .setSecurityEnabled(false)
+            .addAcceptorConfiguration("invm","vm://0"));
+
+        // telling to run the server
+        server.start();
+
+        SpringApplication.run(SfgJmsApplication.class, args);
+    }
+
+}
